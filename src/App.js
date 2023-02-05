ea6be06f076c6158b85a2ae4788c542b6376716b
sinbi <snbiiiii1024@gmail.com>
2023-02-05 21:27:17 +0900

    로고 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./assets/css/index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import mainLogo from "./assets/img/logo.png";
 import Login from "./components/user/LoginPage";
 
 const App = () => {
   return (
     <div className="app">
-      <span className="title">PAWSLY</span>
+      <span className="title">
+        <img src={mainLogo} alt="main Logo" />
+      </span>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
