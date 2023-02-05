0a55050fd28b7b137204a3251114a72c025a28ac
sinbi <snbiiiii1024@gmail.com>
2023-02-05 19:13:33 +0900

    input style 동적 할당 및 click-away-listener 설치

--- src/components/user/LoginPage.js
+++ src/components/user/LoginPage.js
@@ -3,15 +3,17 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import ClickAwayListener from "react-click-away-listener";
 
 const Login = () => {
   const navigate = useNavigate();
-  const state = {
-    clickState: "",
-  };
+  const [clickSection, setClickSection] = useState("");
 
-  const clickInput = (place) => {
-    state.clickState = place;
+  // input 바깥 영역 누르면 모든 active style 해지
+  const handleClickAway = (e) => {
+    if (e.isTrusted) {
+      setClickSection("");
+    }
   };
 
   return (
@@ -20,32 +22,39 @@ const Login = () => {
         <div className="login-section-box-tip"></div>
         <div className="login-section-box-content">
           <span className="login-section-box-content-welcome">Welcome :)</span>
-          <div
-            className={
-              state.clickState === "id"
-                ? "login-section-box-content-input-active"
-                : "login-section-box-content-input"
-            }
-            onClick={() => clickInput("id")}
-          >
-            <input type="text" className="login-section-box-content-input-id" />
-            {/* TODO 정규표현식으로 아이디가 이메일 형식으로 입력됐을 경우에만 아이콘이 나타나도록 */}
-            <FontAwesomeIcon icon={faCheck} className="check-icon" />
-          </div>
-          <div
-            className={
-              state.clickState === "passwd"
-                ? "login-section-box-content-input-active"
-                : "login-section-box-content-input"
-            }
-            onClick={() => clickInput("passwd")}
-          >
-            <input
-              type="password"
-              className="login-section-box-content-input-passwd"
-            />
-            <FontAwesomeIcon icon={faEyeSlash} className="eye-icon" />
-          </div>
+          <ClickAwayListener onClickAway={handleClickAway}>
+            {/*input 영역 하나로 묶기 위한 div*/}
+            <div>
+              <div
+                className={
+                  "login-section-box-content-input" +
+                  (clickSection === "id" ? "-active" : "")
+                }
+              >
+                <input
+                  type="text"
+                  className="login-section-box-content-input-id"
+                  onClick={() => setClickSection("id")}
+                />
+                {/* TODO 정규표현식으로 아이디가 이메일 형식으로 입력됐을 경우에만 아이콘이 나타나도록 */}
+                <FontAwesomeIcon icon={faCheck} className="check-icon" />
+              </div>
+
+              <div
+                className={
+                  "login-section-box-content-input" +
+                  (clickSection === "passwd" ? "-active" : "")
+                }
+              >
+                <input
+                  type="password"
+                  className="login-section-box-content-input-passwd"
+                  onClick={() => setClickSection("passwd")}
+                />
+                <FontAwesomeIcon icon={faEyeSlash} className="eye-icon" />
+              </div>
+            </div>
+          </ClickAwayListener>
         </div>
       </div>
     </div>
