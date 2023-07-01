57e1a072c6376bd54f5368213733b2842084fa24
youseong9201 <83388981+youseong92016873@users.noreply.github.com>
2023-07-02 05:50:50 +0900

    카카오 소셜로그인
    카카오 소셜로그인 토큰 전송됐는지 확인 필요

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./assets/css/index.css";
+import "./assets/css/index.scss";
 import React from "react";
 import Login from "./components/user/LoginPage";
 
 const App = () => {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Login/>
+        </div>
+    );
 };
 
 export default App;
