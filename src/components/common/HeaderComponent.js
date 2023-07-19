3b2cfe17b48697ae9008cd7854f3880fe5ba842d
sinbi <snbiiiii1024@gmail.com>
2023-07-19 23:04:45 +0900

    feat(HeaderComponent): profile 추가
    - profile 버튼 추가
    - profile menu 추가
    - 임시 로그아웃

--- src/components/common/HeaderComponent.js
+++ src/components/common/HeaderComponent.js
@@ -1,17 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBell,
+  faCircleExclamation,
   faEllipsis,
   faGear,
   faPencil,
+  faPowerOff,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { useLocation } from "react-router-dom";
 import mainLogo from "../../public/images/logo.png";
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
   const path = useLocation().pathname;
-  console.log(path);
+  const [profileState, setProfileState] = useState(false);
+
+  const doLogout = () => {
+    window.location = "/";
+  };
 
   return (
     <div
@@ -38,6 +45,44 @@ const Header = () => {
           </div>
           <div className="header-section-item-alert">
             <FontAwesomeIcon icon={faBell} className="alert-icon" />
+          </div>
+          <div className="header-section-item-profile">
+            <FontAwesomeIcon
+              icon={faUser}
+              className="alert-icon"
+              onClick={() => setProfileState(!profileState)}
+            />
+          </div>
+        </div>
+      )}
+      {profileState && (
+        <div className="profile-menu">
+          <div className="profile-menu-user">
+            <div className="profile-menu-user-img">
+              <FontAwesomeIcon icon={faUser} className="alert-icon" />
+            </div>
+            <div className="profile-menu-user-info">
+              <span className="id">John Gnabry</span>
+              <span className="nick">JohnG_7117</span>
+            </div>
+          </div>
+          <div className="profile-menu-item">
+            <div className="profile-menu-item-account">
+              <FontAwesomeIcon icon={faUser} className="account-icon" />
+              <span>계정 관리</span>
+            </div>
+            <div className="profile-menu-item-help">
+              <FontAwesomeIcon
+                icon={faCircleExclamation}
+                className="help-icon"
+              />
+              <span>도움말</span>
+            </div>
+            <div className="sap"></div>
+            <div className="profile-menu-item-logout">
+              <FontAwesomeIcon icon={faPowerOff} className="logout-icon" />
+              <span onClick={doLogout}>로그아웃</span>
+            </div>
           </div>
         </div>
       )}
