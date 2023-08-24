d907302bdafb3f2668bdd3be209633c1b5571289
youseong9201 <83388981+youseong92016873@users.noreply.github.com>
2023-08-25 00:17:23 +0900

    개인피드 미완

--- src/components/user/MyFeedPage.js
+++ src/components/user/MyFeedPage.js
@@ -1,123 +1,109 @@
 import Header from "../common/HeaderComponent";
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-  faBars,
-  faChartSimple,
-  faMagnifyingGlass,
+    faBars,
+    faChartSimple,
+    faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 import parkImg from "../../public/images/park-img.png";
 import dudImg from "../../public/images/dud-img.jpg";
 import categoryIcon from "../../public/images/btn_icon.png";
+import {apiClient} from "../utils/api";
+import axios from "axios";
 
 const MyFeed = () => {
-  const [userData, setUserData] = useState("");
-  const [openState, setOpenState] = useState(false);
-  const boardList = [
-    "친환경 제품을 사용한 기업 제품 소개",
-    "지속 가능한 디자인을 위해서 무엇을 해야할까",
-    "지속가능 디자인 수요&발전 조사서 정리",
-    "모르면 후회하는 친환경 실내디자인 사례와 진행 방향",
-    "2023 디자인페스티벌, 아기자기 악세서리로 재탄생하다",
-    "2023년 떠오르고 있는 신예 작가 전시전, 특전 정보 공유",
-  ];
-  const mainCategoryList = [
-    { IT: "IT 소식" },
-    { FRONT: "프론트엔드" },
-    { BACK: "백엔드" },
-    { DESIGN: "디자인" },
-    { PORTFOLIO: "포트폴리오" },
-  ];
+    const [userData, setUserData] = useState("");
+    const [openState, setOpenState] = useState(false);
+    const [boardList, setBoardList] = useState([]);
 
-  useEffect(() => {
-    const userInfo = localStorage.getItem("userData");
 
-    if (userInfo) {
-      setUserData(JSON.parse(userInfo));
-    }
-  }, []);
+    useEffect(() => {
+        const userInfo = localStorage.getItem("userData");
 
-  return (
-    <div className="my-section">
-      <Header />
-      <FontAwesomeIcon
-        icon={faBars}
-        className="category"
-        onClick={() => setOpenState(true)}
-      />
-      <div
-        className={
-          openState
-            ? "my-section-category-opened"
-            : "my-section-category-closed"
+        if (userInfo) {
+            setUserData(JSON.parse(userInfo));
         }
-      >
-        <div className="my-section-category-opened-header">
-          <div
-            className="my-section-category-opened-header-icon"
-            onClick={() => setOpenState(false)}
-          >
-            <FontAwesomeIcon icon={faChartSimple} />
-          </div>
-          <div className="my-section-category-opened-header-menu">
-            <div className="my-section-category-opened-header-menu-category">
-              <i className="fa-regular fa-file-lines"></i>
-              <span>내 카테고리 관리</span>
+        axios.get("http://3.39.25.7:8080/board/list/{writer}")
+            .then(response => {
+                setBoardList(response.data);
+
+
+            })
+
+            .catch(error => {
+                console.error("데이터 가져오기 실패:", error);
+            });
+    }, []);
+
+    // const currentUserBoardList = boardList.filter(
+    //     (post) => post.writer === userData.nickname
+    // );
+
+
+    return (
+        <div className="my-section">
+            <Header/>
+            <FontAwesomeIcon
+                icon={faBars}
+                className="category"
+                onClick={() => setOpenState(true)}
+            />
+            <div
+                className={
+                    openState
+                        ? "my-section-category-opened"
+                        : "my-section-category-closed"
+                }
+            >
+                <div className="my-section-category-opened-header">
+                    <div
+                        className="my-section-category-opened-header-icon"
+                        onClick={() => setOpenState(false)}
+                    >
+                        <FontAwesomeIcon icon={faChartSimple}/>
+                    </div>
+                    <div className="my-section-category-opened-header-menu"></div>
+                </div>
+                <div className="my-section-category-opened-body">
+                    <div className="my-section-category-opened-body-first"></div>
+                    <div className="my-section-category-opened-body-second"></div>
+                    <div className="my-section-category-opened-body-third"></div>
+                </div>
+                <div className="my-section-category-opened-footer">
+                    <div className="my-section-category-opened-footer-delete"></div>
+                    <div className="my-section-category-opened-footer-create"></div>
+                    <div className="my-section-category-opened-footer-edit"></div>
+                </div>
             </div>
-            <i className="fa-solid fa-arrow-down-wide-short"></i>
-          </div>
-        </div>
-        <div className="my-section-category-opened-body">
-          <div className="my-section-category-opened-body-first"></div>
-          <div className="my-section-category-opened-body-second">
-            {mainCategoryList.map((category, index) => (
-              <div className="my-section-category-opened-body-second-item">
-                <img src={categoryIcon} alt="category icon" />
-                <span>{Object.values(category)}</span>
-              </div>
-            ))}
-          </div>
-          <div className="my-section-category-opened-body-third"></div>
-        </div>
-        <div className="my-section-category-opened-footer">
-          <div className="my-section-category-opened-footer-delete">
-            <span>삭제</span>
-          </div>
-          <div className="my-section-category-opened-footer-create">
-            <span>카테고리 생성</span>
-            <i className="fa-solid fa-plus"></i>
-          </div>
-          <div className="my-section-category-opened-footer-edit">
-            <span>카테고리 수정</span>
-            <i className="fa-solid fa-plus"></i>
-          </div>
-        </div>
-      </div>
 
-      <span className="sub">3D Max모델링 디자인 스터디</span>
-      <span className="main">{userData.nickname}의 글</span>
+            <span className="sub">3D Max모델링 디자인 스터디</span>
+            <span className="main">{userData.nickname}의 글</span>
 
-      <div className="my-section-search">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-        <input type="text" placeholder="검색어를 입력하세요." />
-      </div>
 
-      <div className="my-section-list">
-        <div className="my-section-list-sort">최신글</div>
-        {boardList.map((board, index) => (
-          <div key={index} className="my-section-list-item">
-            <div className="my-section-list-item-img">
-              <img
-                src={userData.userid === "parkgun" ? parkImg : dudImg}
-                alt="test img"
-              />
+            <div className="my-section-search">
+                <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                <input type="text" placeholder="검색어를 입력하세요."/>
             </div>
-            <div className="my-section-list-item-title">{board}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+            <div className="my-section-list">
+                <div className="my-section-list-sort">최신글</div>
+                {boardList.length === 0 ? (
+                    <div className="my-section-list-empty">게시물이 없습니다.</div>
+                ) : (
+
+                    boardList.slice(0, 6).map((board) => (
+                        <div key={board.boardKey} className="my-section-list-item">
+                            <div className="my-section-list-item-img">
+                                {/* 이미지 렌더링 */}
+                            </div>
+                            <div className="my-section-list-item-title">{board.title}</div>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default MyFeed;
